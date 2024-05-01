9265f531e06a1e24c84b881a3816eff9694865c5
james.wood <jwooddev@gmail.com>
2024-04-30 21:02:57 -0500

    todo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "engine.h"
 
-// TODO: Convert to raylib.
+// TODO: Convert to raylib. Final pass needed, game runs. Need to double-check collision logic (and
+// other things).
 // TODO: Rotation for crab based on speed?
 // TODO: Animations on claws?
 int main() {
